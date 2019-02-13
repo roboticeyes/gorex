6987d3fdbf926d5a246378a49da948fbd0c959d7
Bernhard Reitinger <br@reecon.at>
2019-02-13 16:08:58 +0100

    Code cleanup

--- cmd/rxc/main.go
+++ cmd/rxc/main.go
@@ -185,46 +185,4 @@ func main() {
 	default:
 		help(1)
 	}
-
-	// // Setup REX client connection
-	// apiURL := baseURL + "/rex-gateway/api/v2"
-	// tokenURL := baseURL
-	// cli := gorex.NewRexClient(tokenURL, apiURL, apiURL)
-	//
-	// _, err := cli.ConnectWithClientCredentials(clientID, clientSecret)
-	// if err != nil {
-	// 	log.Fatal("Error during connection", err)
-	// }
-	//
-	// // Setup services
-	//
-	// projectService := gorex.NewProjectService(cli)
-	// userService := gorex.NewUserService(cli)
-	//
-	// user, err := userService.GetCurrentUser()
-	// if err != nil {
-	// 	fmt.Println("Cannot get user", err)
-	// }
-	// fmt.Println(user)
-	//
-	// name := "test"
-	// owner := user.UserID
-	// project, err := projectService.FindByNameAndOwner(name, owner)
-	//
-	// if err != nil {
-	// 	fmt.Println("Cannot get project", err)
-	// }
-	//
-	// fmt.Println(project)
-	//
-	// // Add project file
-	// r, _ := os.Open("/tmp/test.rex")
-	// defer r.Close()
-	//
-	// ft := gorex.NewFileTransform()
-	//
-	// err = projectService.UploadProjectFile(*project, "testProjectFile", "test.rex", ft, r)
-	// if err != nil {
-	// 	fmt.Println("Cannot upload file", err)
-	// }
 }
