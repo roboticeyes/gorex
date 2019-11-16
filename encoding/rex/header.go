7ad7fc4f1ff6b0c6fa81694d51baf85a8284f770
Bernhard Reitinger <br@reecon.at>
2019-11-16 14:27:54 +0100

    remove scenegraph in rex file

--- encoding/rex/header.go
+++ encoding/rex/header.go
@@ -20,19 +20,17 @@ const (
 	typePeopleSimulation = 6
 	typeUnityPackage     = 7
 	typeSceneNode        = 8
-	typeSceneGraph       = 9
 )
 
 // Header defines the structure of the REX header
 type Header struct {
-	Magic          [4]byte
-	Version        uint16
-	Crc            uint32
-	NrBlocks       uint16
-	StartAddr      uint16
-	SizeBytes      uint64
-	StartScenGraph uint64
-	Reserved       [34]byte
+	Magic     [4]byte
+	Version   uint16
+	Crc       uint32
+	NrBlocks  uint16
+	StartAddr uint16
+	SizeBytes uint64
+	Reserved  [42]byte
 }
 
 // DataBlockHeader stores the header information of a data block
@@ -46,12 +44,11 @@ type DataBlockHeader struct {
 // CreateHeader returns a valid fresh header block
 func CreateHeader() *Header {
 	header := &Header{
-		Version:        1,
-		Crc:            0,
-		NrBlocks:       0,
-		StartAddr:      86, // fixed CSB of 22 bytes
-		SizeBytes:      0,
-		StartScenGraph: 0,
+		Version:   1,
+		Crc:       0,
+		NrBlocks:  0,
+		StartAddr: 86, // fixed CSB of 22 bytes
+		SizeBytes: 0,
 	}
 	header.Magic[0] = 'R'
 	header.Magic[1] = 'E'
@@ -70,7 +67,6 @@ func (h *Header) Write(w io.Writer) error {
 		h.NrBlocks,
 		h.StartAddr,
 		h.SizeBytes,
-		h.StartScenGraph,
 		h.Reserved,
 		// default CSB block
 		uint32(3876),
